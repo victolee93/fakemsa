da814c8bfea0f67cfbab11212904444a369098e9
victolee <victolee93@gmail.com>
2020-08-17 15:34:35 +0900

    init

---
+++ src/main/java/com/victolee/fakemsa/FakemsaApplication.java
@@ -0,0 +1,13 @@
+package com.victolee.fakemsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakemsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakemsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victolee/fakemsa/FakemsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victolee.fakemsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakemsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
